fbe6213a410a7d6150fa22ab754c82a4723db24d
t-eri <tkymework@gmail.com>
2025-05-16 11:53:39 +0900

    [fix]ex04に実装ミスがあったため修正

--- ch04/ex04/index.ts
+++ ch04/ex04/index.ts
@@ -5,18 +5,33 @@
  * bitCount(0b1111111111111111111111111111111) は 31 を返しなさい。
  */
 
+export function bitCount(n: number): number {
+  let count = 0;
+  let num = 1;
+
+  for (let i = 0; i < 32; i++) {
+    if (n & num) {
+      count++;
+    }
+    num = num << 1;
+  }
+  return count;
+}
+
+// 文字列比較してしまったため、修正
 // 考え方
 // 32ビット整数表現に変換する
 // 32ビット整数表現形式の文字数を数える
 // 一文字ずつ確認し、1の場合にカウントをインクリメント
-export function bitCount(n: number): number {
-  // >> 符号付き右シフト演算子を使って、符号付32ビット整数表現として扱う
-  const binaryString = (n >> 0).toString(2); // 2進数文字列に変換
-  let count = 0;
-  for (let i = 0; i < binaryString.length; i++) {
-    if (binaryString[i] === '1') {
-      count++;
-    }
-  }
-  return count;
-}
+
+// export function bitCount(n: number): number {
+//   // >> 符号付き右シフト演算子を使って、符号付32ビット整数表現として扱う
+//   const binaryString = (n >> 0).toString(2); // 2進数文字列に変換
+//   let count = 0;
+//   for (let i = 0; i < binaryString.length; i++) {
+//     if (binaryString[i] === '1') {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
